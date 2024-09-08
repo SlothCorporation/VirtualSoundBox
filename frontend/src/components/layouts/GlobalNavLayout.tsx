ed53da865dfc023aebbba1c96d0d2b0eb5f28f73
Ryota Nozawa <noza@RyotanoMacBook-Pro.local>
2024-09-08 21:27:41 +0900

    article layout

--- frontend/src/components/layouts/GlobalNavLayout.tsx
+++ frontend/src/components/layouts/GlobalNavLayout.tsx
@@ -3,50 +3,55 @@ import {MdMenu} from 'react-icons/md';
 import Link from "next/link";
 
 interface GlobalNavLayoutProps {
-    children: React.ReactNode;
-    sideNavContent: React.ReactNode;
+  children: React.ReactNode;
+  sideNavContent: React.ReactNode;
 }
 
 export function GlobalNavLayout({
-    sideNavContent,
-    children }: GlobalNavLayoutProps) {
-    const [isSideNavOpen, setIsSideNavOpen] = useState(false);
-    return (
-        <div className="flex flex-col h-screen">
-            <header className="mb-12">
-                <nav className="fixed top-0 inset-x-0 h-12 bg-black flex items-center justify-between z-50">
-                    <div className="pl-14">
-                        <div className="flex items-center space-x-4">
-                            <Link href="/">
-                                <div className="flex items-center leading-[0] space-x-4">
-                                    VtuberDatabase
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="ml-auto flex items-center">
-                        <button
-                            type="button"
-                            onClick={(e) => {
-                                setIsSideNavOpen(!isSideNavOpen);
-                                e.stopPropagation();
-                            }}
-                            className="flex items-center px-2 h-12 hover:bg-[#393939]"
-                        >
-                            <MdMenu color="white" size={24} />
-                        </button>
-                        <aside
-                            className={`bg-white text-black z-50 fixed top-12 right-0 shadow-[0_2px_4px_rgba(0,0,0,0.15)] h-[calc(100vh_-_3rem)] overflow-y-auto transition-all duration-150 ease-in-out flex flex-col
+  sideNavContent,
+  children
+}: GlobalNavLayoutProps) {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="mb-12 w-full flex justify-center">
+        <nav className="fixed top-0 inset-x-0 h-12 bg-black flex items-center justify-between z-50 w-full px-4">
+          <div>
+            <div className="flex items-center space-x-4">
+              <Link href="/">
+                <div className="flex items-center leading-[0] space-x-4">
+                  VirtualSoundBox
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="ml-auto flex items-center">
+            <button
+              type="button"
+              onClick={(e) => {
+                setIsSideNavOpen(!isSideNavOpen);
+                e.stopPropagation();
+              }}
+              className="flex items-center h-12 hover:bg-[#393939]"
+            >
+              <MdMenu color="white" size={24}/>
+            </button>
+            <aside
+              className={`bg-white text-black z-50 fixed top-12 right-0 shadow-[0_2px_4px_rgba(0,0,0,0.15)] h-[calc(100vh_-_3rem)] overflow-y-auto transition-all duration-150 ease-in-out flex flex-col
                             ${isSideNavOpen ? 'w-[320px]' : 'w-0'}`}
-                        >
-                            {sideNavContent}
-                        </aside>
-                    </div>
-                </nav>
-            </header>
-            {children}
+            >
+              {sideNavContent}
+            </aside>
+          </div>
+        </nav>
+      </header>
+      <main className="flex-1 w-full flex justify-center bg-white text-black">
+        <div className="w-full max-w-[1280px] px-4">
+          {children}
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default GlobalNavLayout;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,57 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+import Image from 'next/image';
+import { SlArrowRight } from "react-icons/sl";
 
 export default function Home() {
-    return (
-        <Layout>
-            <div>これから実装</div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div>これから実装</div>
+      <div className="border rounded">
+        <div>トピック</div>
+        <div className="p-5">
+          <div className="border rounded flex">
+            <div><Image src="/art.jpeg" width={100} height={100}/></div>
+            <div className="p-2">
+              <div className="border-b w-full">アーティスト名</div>
+              <div>紹介文</div>
+            </div>
+          </div>
+          <div className="border rounded flex">
+            <div><Image src="/art.jpeg" width={100} height={100}/></div>
+            <div className="p-2">
+              <div className="border-b w-full">アーティスト名</div>
+              <div>紹介文</div>
+            </div>
+          </div>
+          <div className="border rounded flex">
+            <div><Image src="/art.jpeg" width={100} height={100}/></div>
+            <div className="p-2">
+              <div className="border-b w-full">アーティスト名</div>
+              <div>紹介文</div>
+            </div>
+          </div>
+          <div className="border rounded flex">
+            <div><Image src="/art.jpeg" width={100} height={100}/></div>
+            <div className="p-2">
+              <div className="border-b w-full">アーティスト名</div>
+              <div>紹介文</div>
+            </div>
+          </div>
+          <div className="border rounded flex">
+            <div><Image src="/art.jpeg" width={100} height={100}/></div>
+            <div className="p-2">
+              <div className="border-b w-full">アーティスト名</div>
+              <div>紹介文</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end border-t">
+          <Link href="/articles">
+            <div className="flex items-center text-xs p-1">もっと見る<SlArrowRight/></div>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
 }
