fc06bba7c801ce235db719d0f27fd0a32d9b6a50
Ryota Nozawa <noza@RyotanoMacBook-Pro.local>
2025-04-02 15:02:11 +0900

    auth apiの実装

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\FetchYoutubeDataController;
 use App\Http\Controllers\Api\FetchMusicDataController;
 use App\Http\Controllers\Api\SetMusicDataController;
@@ -17,10 +18,13 @@ use App\Http\Controllers\Api\SetMusicDataController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/youtube', FetchYoutubeDataController::class);
 Route::get('/music', FetchMusicDataController::class);
 Route::post('/music', SetMusicDataController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
