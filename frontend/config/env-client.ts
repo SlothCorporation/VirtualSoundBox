df3f19ae0521a9255b7e19b86444816ed358bb43
Ryota Nozawa <noza@RyotanoMacBook-Pro.local>
2025-08-03 03:26:52 +0900

    gaの設定

--- frontend/config/env-client.ts
+++ frontend/config/env-client.ts
@@ -1,2 +1,5 @@
 export const NEXT_PUBLIC_BACKEND_URL = process.env
   .NEXT_PUBLIC_BACKEND_URL as string;
+
+export const NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = process.env
+  .NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string;
