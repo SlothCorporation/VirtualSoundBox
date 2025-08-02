27768c82d7084bf3cc9c042bc9bb19c5b24c32e6
なまけもの商事 <hutisaki.aoi+xxx1@gmail.com>
2025-08-03 03:30:52 +0900

    gaの設定 (#61)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,30 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import ToastProvider from "@/components/Toast/Toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Script from "next/script";
+import { NEXT_PUBLIC_GOOGLE_ANALYTICS_ID } from "@/config/env-client";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `,
+        }}
+      />
       <QueryClientProvider client={queryClient}>
         <ToastProvider />
         <Component {...pageProps} />
