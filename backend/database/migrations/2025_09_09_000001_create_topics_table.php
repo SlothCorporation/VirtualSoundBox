c148149838f11360963560d1c75ce7e23c63b096
Ryota Nozawa <noza@RyotanoMacBook-Pro.local>
2025-09-16 00:30:05 +0900

    migrateファイルの定義

---
+++ backend/database/migrations/2025_09_09_000001_create_topics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('article_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->unsignedInteger('position')->default(1); // 並び順
+            $table->timestamps();
+
+            $table->unique('article_id'); // 同じ記事を複数回登録させない
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('topics');
+    }
+};
