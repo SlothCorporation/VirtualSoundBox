3035b632069dd398f3d4e8a0f1c9cebe9909065f
Ryota Nozawa <noza@RyotanoMacBook-Pro.local>
2025-10-19 04:23:42 +0900

    各種データリソースからデータ取得するserviceを実装

---
+++ backend/app/Services/Analytics/DbService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services\Analytics;
+
+use Illuminate\Support\Collection;
+
+class DbService
+{
+    public static function fetch(string $startDate, string $endDate): array
+    {
+        return [];
+    }
+}
